f0a3626a44627b924e8296834293a8971344a6c5
Jonatan <69942667+somesoo@users.noreply.github.com>
2022-01-21 00:19:10 +0100

    Update lfu.py

--- lfu.py
+++ lfu.py
@@ -1,8 +1,10 @@
 class LFU:
+    # define some variable, and arrays
     queue = []
     unique_list = []
     current_frame_status = [[0 for x in range(2)] for x in range(3)]
     changes = 0
+    # class constructor that gets the site sequence from generator, and generate the unique list of sites in range of sequence length
     def __init__(self, table):
         self.queue = table
         for x in range(len(table)):
@@ -10,7 +12,8 @@ class LFU:
             temp[0] = x
             if temp not in self.unique_list:
                 self.unique_list.append(temp)
-
+    
+    # choosing the smallest number of frequency usage
     def choose_smalest(self, status):
         if status[0][1] <= status[1][1] and status[0][1] <= status[2][1]:
             return 0
@@ -21,6 +24,7 @@ class LFU:
         else:
             return -1
 
+    # checking if the site is alrady in frame
     def check_if_exist(self, table, curr_time):
         for i in self.current_frame_status:
             if int(i[0]) == int(self.queue[curr_time][0]):
@@ -28,7 +32,8 @@ class LFU:
                 i[1] = self.unique_list[int(self.queue[curr_time][0])][1]
                 return 1
         return 0
-
+    
+    # update the frame
     def update_next_frame(self, table, curr_time):
         if LFU.check_if_exist(self, table, curr_time):
             pass
