e380c3cd272ff9448588ce461386b78fe08a842a
Jonatan <69942667+somesoo@users.noreply.github.com>
2022-01-21 00:13:10 +0100

    add comments

--- lru.py
+++ lru.py
@@ -1,10 +1,13 @@
 class LRU:
+    # define some variables
     queue = []
     current_frame_status = [[0 for x in range(2)] for x in range(3)]
     changes = 0
+    # class constructor that assign table of sites to class list
     def __init__(self, table):
         self.queue = table
 
+    # function to return the element with smallest recent use status
     def choose_smalest(self, status):
         if status[0][1] <= status[1][1] and status[0][1] <= status[2][1]:
             return 0
@@ -14,14 +17,14 @@ class LRU:
             return 2
         else:
             return -1
-
+    # check if the site is already in frame
     def check_if_exist(self, table, curr_time):
         for i in self.current_frame_status:
             if i[0] == int(self.queue[curr_time][0]):
                 i[1] = curr_time+1
                 return 1
         return 0
-
+    # update next frame
     def update_next_frame(self, table, curr_time):
         if LRU.check_if_exist(self, table, curr_time):
             pass
